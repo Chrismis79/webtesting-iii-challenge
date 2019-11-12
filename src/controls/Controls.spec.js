3eba960f6f5fc999885e4c19a146831a1060f8af
Chrismis79 <chrismis79@gmail.com>
2019-11-12 10:52:38 -0600

    MVP complete, I think

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,53 @@
 // Test away!
+//test if component renders
+//snapshot test
+//fire event test to see if toggleLocked and toggleClosed are being called.
+//can you test if classes are toggling?
+
+import React from 'react';
+import {render, fireEvent, cleanup} from '@testing-library/react';
+import Controls from '../controls/Controls';
+
+
+afterEach(cleanup);
+
+test('Controls renders correctly', () => {
+    expect(render(<Controls/>)).toMatchSnapshot()
+});
+
+//tests if buttons rendering
+test('Buttons rendering correctly', () => {
+    
+    const toggleLockMock = jest.isMockFunction(() => !locked);
+    const toggleClosedMock = jest.fn(() => !closed);
+    const {getByText} = render(<Controls locked={false} closed={false} toggleLock={toggleLockMock} toggleClosed={toggleClosedMock}/>)
+    getByText(/lock gate/i);
+    getByText(/close gate/i);
+})
+
+test('close gate button triggers toggle close', async ()=> {
+    const toggleClosedMock = jest.fn();
+    const {findByText} = render(
+        <Controls toggleClosed={toggleClosedMock} closed={false}/>
+    );
+    const closeButton = findByText((/close gate/i));
+    await fireEvent(closeButton, {target: closeButton});
+        
+    expect(toggleClosedMock).toHaveBeenCalled();
+    expect(toggleClosedMock).toHaveBeenCalledTimes(1);
+});
+
+test('Closed button disabled if gate is locked', () => {
+    const locked = true;
+    const closed = false;
+    const toggleLockMock = jest.fn(() => !locked);
+    const toggleClosedMock = jest.fn(() => !closed);
+    const {getByText} = render(
+        <Controls
+        locked={locked}
+        closed={closed}
+        toggleLock={toggleLockMock}
+        toggleClosed={toggleClosedMock} />
+    )
+    expect(getByText(/close gate/i)).
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,19 @@
 // Test away
+//test if component renders without crashing
+//snapshot test
+//test toggleLocked is calling setState and changing it from locked to unlocked
+//same for toggleClosed
+import React from 'react';
+import {render, cleanup} from '@testing-library/react';
+import Dashboard from '../dashboard/Dashboard';
+import '@testing-library/jest-dom/extend-expect';
+
+
+afterEach(cleanup);
+
+//tests if Dashboard is rendering
+test("Dashboard is rendering correctly", () => {
+    expect(render(<Dashboard/>)).toMatchSnapshot();
+});
+
+
